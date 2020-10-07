f3dc26eb52d3439ffcbf44ec09e22a31b0be839e
Craig Swanson <cswanson02@me.com>
2020-10-07 17:38:10 -0500

    Refactored eating_cookies to make cache implementation match test cases.

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,15 +2,17 @@
 Input: an integer
 Returns: an integer
 '''
-def eating_cookies(n, cache = {}):
+def eating_cookies(n, cache_array = []):
     # Your code here
+    if cache_array == []:
+        cache_array = [0 for i in range(n+1)]
     if n < 0:
         return 0
     if n == 0:
         return 1
-    if n not in cache:
-        cache[n] = eating_cookies(n - 1, cache) + eating_cookies(n - 2, cache) + eating_cookies(n - 3, cache)
-    return cache[n]
+    if cache_array[n-1] == 0:
+        cache_array[n-1] = eating_cookies(n - 1, cache_array) + eating_cookies(n - 2, cache_array) + eating_cookies(n - 3, cache_array)
+    return cache_array[n-1]
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
